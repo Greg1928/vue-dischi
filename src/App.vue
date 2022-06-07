f907a67840eeb6406648c4a0a0f5b3ae5bdcef37
Greg1928 <gregoriosortino@protonmail.com>
2022-06-07 15:48:07 +0200

    added header

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,21 @@
 <template>
   <div id="app">
+    <BaseHeader/>
   </div>
 </template>
 
 <script>
+import BaseHeader from './components/blocks/BaseHeader.vue';
 
 
 export default {
   name: 'App',
   components: {
-  
-  }
+    BaseHeader
+}
 }
 </script>
 
 <style lang="scss">
-
+@import './assets/style/global.scss';
 </style>
--- src/components/blocks/BaseHeader.vue
+++ src/components/blocks/BaseHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <header>
-
+      <img src="../../assets/img/spotify-logo.png" alt="Spotify Logo">
   </header>
 </template>
 
@@ -11,5 +11,12 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+header{
+    background-color: var(--secondary-color);
 
+    img{
+       height: 4rem;
+       margin: 1.3rem;
+    }
+}
 </style>
